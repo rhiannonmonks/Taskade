6520d01880fb497a95465705a0fa466b4ef08003
Rhiannon Monks <rhiannon.monks@gmail.com>
2021-06-10 17:14:34 +0930

    setup signup and signin mutations, encrypt password, saved user in db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
+const bcrypt = require('bcryptjs');
 dotenv.config();
 
 const { DB_URI, DB_NAME } = process.env;
@@ -18,7 +19,29 @@ const typeDefs = gql`
   type Query{
     myTaskLists: [TaskList!]
   }
+
+  type Mutation {
+    signUp(input: SignUpInput): AuthUser!
+    signIn(input: SignInInput): AuthUser!
+  }
+
+  input SignUpInput {
+    email: String!, 
+    password: String!, 
+    name: String!, 
+    avatar: String
+  }
+
+  input SignInInput {
+    email: String!
+    password: String!
+  }
   
+  type AuthUser {
+    user: User!
+    token: String!
+  }
+
   type User {
     id: ID!
     name: String!
@@ -49,6 +72,30 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     myTaskLists: () => []
+  },
+  Mutation: {
+    signUp: async (_, { input }, { db }) => {
+      const hashedPassword = bcrypt.hashSync(input.password);
+      const newUser = {
+        ...input,
+        password: hashedPassword,
+      }
+      // save to database
+      const result = await db.collection('Users').insert(newUser);
+      const user = result.ops[0]
+      return {
+        user,
+        token: 'token'
+      }
+    },
+
+    signIn: () => {
+
+    },
+  },
+
+  User: {
+    id: ({_id, id}) =>_id|| id
   }
 };
 
